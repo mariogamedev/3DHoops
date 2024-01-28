5924ac476d4930f71fa3b1690e0a16af0ea92620
mariogamedev <mariogamedevv@gmail.com>
2024-01-28 23:38:17 +0100

    Add animations for the baller animator

---
+++ Assets/Scripts/Baller/BallerState/PickUpBallState.cs
@@ -0,0 +1,56 @@
+
+using UnityEngine;
+
+namespace Baller
+{
+    public class PickUpBallState : BaseBallerState
+    {
+        private const float PICKUP_TIMEOUT = 0.6f;
+  
+        private float _elapsedTime = 0f;
+        private Ball _ball;
+        private bool _handledBall = false;
+
+        public PickUpBallState(Baller baller, Ball ball) 
+        { 
+            Baller = baller;
+            _ball = ball;
+            AnimationTag = "PickUp";
+        }
+
+        public override void EnterState()
+        {
+            base.EnterState();
+            Reset();
+        }
+
+        private void Reset()
+        {
+            _elapsedTime = 0f;
+            _handledBall = false;
+        }
+
+        public override void UpdateState()
+        {
+            if (!_handledBall)
+            {
+                _ball.EnableKinematic();
+                _ball.SetPosition(Baller.HandPosition);
+                _handledBall = true;
+            }
+
+            if (_elapsedTime >= PICKUP_TIMEOUT)
+            {
+                BallerInputNotifications.StartDribbleAction.Invoke();
+            }
+
+            _elapsedTime += Time.deltaTime;
+        }
+
+        public override void ExitState()
+        {
+            base.ExitState();
+            _ball.DisableKinematic();
+        }
+    }
+}
