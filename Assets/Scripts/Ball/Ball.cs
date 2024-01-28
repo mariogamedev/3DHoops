5b8e383b16557f4ea8d13550aada4c91655a36c3
mariogamedev <mariogamedevv@gmail.com>
2024-01-28 23:34:24 +0100

    Adjust ball bounciness, transform, plus add some rigidbody, position contro logic

--- Assets/Scripts/Ball/Ball.cs
+++ Assets/Scripts/Ball/Ball.cs
@@ -10,5 +10,20 @@ namespace Baller
         {
             _rigidbody = GetComponent<Rigidbody>();
         }
+
+        public void EnableKinematic()
+        {
+            _rigidbody.isKinematic = true;
+        }
+
+        public void DisableKinematic()
+        {
+            _rigidbody.isKinematic = false;
+        }
+
+        public void SetPosition(Vector3 position)
+        {
+            transform.position = position;
+        }
     }
 }
